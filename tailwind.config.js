f3aa37c5ce1ed6e30c5a8b729324732660f07360
Oleksii Shpak <willwildred@gmail.com>
2024-07-18 22:24:05 -0600

    new main, new design overall

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,35 @@ module.exports = {
     extend: {
       animation: {
         sphere: "sphere 7s infinite",
+        stars: "stars 4s infinite",
       },
 
       keyframes: {
         sphere: {
           "0%": {
-            transform: "translate(0px, 0px) scale(1)"
+            transform: "translate(0px, 0px) scale(1)",
           },
           "33%": {
-            transform: "translate(40px, -50px) scale(1.1)"
+            transform: "translate(40px, -50px) scale(1.1)",
           },
           "66%": {
-            transform: "translate(50px, 40px) scale(0.9)"
+            transform: "translate(50px, 40px) scale(0.9)",
           },
           "100%": {
-            transform: "translate(0px, 0px) scale(1)"
+            transform: "translate(0px, 0px) scale(1)",
           },
+        },
+
+        stars: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "50%": {
+            transform: "translate(1200px, -20px) scale()",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          }
         }
       }
     },
